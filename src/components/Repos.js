ae824895edfa35e5963e9dbed4aa846ebd27da25
Tan Minh <tmt.study.27@gmail.com>
2021-12-03 11:04:37 +0700

    test

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,9 +1,43 @@
-import React from 'react';
-import styled from 'styled-components';
-import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import React from "react";
+import styled from "styled-components";
+import { GithubContext } from "../context/context";
+import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const { repos } = React.useContext(GithubContext);
+
+  const pieChart = (data) => {
+    let obj = {};
+    data.map((item) => {
+      if (item.language) {
+        obj[item.language]
+          ? (obj[item.language] = {
+              ...obj[item.language],
+              value: obj[item.language].value + 1,
+            })
+          : (obj[item.language] = { label: item.language, value: 1 });
+      } else {
+        obj["Others"]
+          ? (obj["Others"] = {
+              ...obj["Others"],
+              value: obj["Others"].value + 1,
+            })
+          : (obj["Others"] = {
+              label: "Others",
+              value: 1,
+            });
+      }
+    });
+
+    return Object.values(obj).sort((a, b) => b.value - a.value);
+  };
+
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <Pie3D data={pieChart(repos)} />
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
